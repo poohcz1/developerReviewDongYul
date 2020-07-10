bec4736588bfc8bb04f5e8de564ddb6e230ed50a
poohcz1 <zergkdl@naver.com>
2020-07-10 10:10:59 +0900

    line change

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ const port = 5000;
 app.set('views', path.join(__dirname, '/'))
 app.set('view engine', 'html');
 
+
 app.use(express.static('public'));
  
 http.createServer(app).listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}/`)
-});
+});   
